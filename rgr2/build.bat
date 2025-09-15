ddd335f547dc61bd492e87361123190724323888
GoldmanYT <89934420+GoldmanYT@users.noreply.github.com>
2025-09-15 11:52:19 +0300

    Update build.bat

--- rgr2/build.bat
+++ rgr2/build.bat
@@ -5,21 +5,16 @@ set obj_path=%name%.obj
 set exe_path=%name%.exe
 set output_path=%folder%
 
-cd ..
 :: Создание объектного файла
-ml64 /Cp /c %asm_path%
+..\ml64 /Cp /c %asm_path%
 pause
 
 :: Создание исполняемого файла
-link /SUBSYSTEM:CONSOLE /ENTRY:WinMain %obj_path%
+..\link /SUBSYSTEM:CONSOLE /ENTRY:WinMain %obj_path%
 pause
 
 :: Удаление объектного файла
 del /q %obj_path%
-
-:: Перемещение исполняемого файла
-move /Y %exe_path% %output_path%
-cd %folder%
 
 %exe_path%
 pause
